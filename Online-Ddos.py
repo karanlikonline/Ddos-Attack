112a54ca7675c3e0353f2a614a7bad294bf58d5e
karanlikonline <74505456+karanlikonline@users.noreply.github.com>
2020-11-16 09:31:35 +0300

    Update Online-Ddos.py

--- Online-Ddos.py
+++ Online-Ddos.py
@@ -20,7 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "instagram   : instagram.com/karanlik.online"
+print "İnstagram  : https://www.instagram.com/karanlik.online"
 print
 ip = input("Hedef IP : ")
 port = input("Port : ")
